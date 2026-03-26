1e2e90f70164cc454ed2d448d0a11f98329375d8
eugelu11 <34776849+eugelu11@users.noreply.github.com>
2026-03-25 22:01:45 -0300

    cambios para ajustar según TP

--- cliente.py
+++ cliente.py
@@ -1,20 +1,37 @@
 import socket
 
-
 HEADER = 64
-PORT = 5050
-SERVER =socket.gethostbyname(socket.gethostname())
+PORT = 5000
+SERVER = "127.0.0.1"
 FORMAT = 'utf-8'
-MENSAJE_DESCONEXION = "!DESCONECTAR"
-ADDR=(SERVER,PORT)
+MENSAJE_DESCONEXION = "exito"
 
-cliente=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ADDR = (SERVER, PORT)
+
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cliente.connect(ADDR)
 
 def mandar(mensaje):
-    mensaje=mensaje.encode(FORMAT)
-    len_mensaje=len(mensaje)
-    mandar_len=str(len_mensaje).encode(FORMAT)
+    mensaje_bytes = mensaje.encode(FORMAT)
+    len_mensaje = len(mensaje_bytes)
+
+    mandar_len = str(len_mensaje).encode(FORMAT)
     mandar_len += b' ' * (HEADER - len(mandar_len))
-    cliente.send(mandar_len)
-    cliente.send(mensaje)
+
+    cliente.sendall(mandar_len)
+    cliente.sendall(mensaje_bytes)
+
+    len_respuesta = cliente.recv(HEADER).decode(FORMAT)
+    len_respuesta = int(len_respuesta.strip())
+
+    respuesta = cliente.recv(len_respuesta).decode(FORMAT)
+    print(respuesta)
+
+while True:
+    mensaje = input(">> ")
+    mandar(mensaje)
+
+    if mensaje == MENSAJE_DESCONEXION:
+        break
+
+cliente.close()
