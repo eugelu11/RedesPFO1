f0ddb809d8f7e22276505d71d3e42434f15fe681
eugelu11 <34776849+eugelu11@users.noreply.github.com>
2026-03-25 21:53:48 -0300

    final server y cli

--- cliente.py
+++ cliente.py
@@ -4,6 +4,17 @@ import socket
 HEADER = 64
 PORT = 5050
 SERVER =socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 MENSAJE_DESCONEXION = "!DESCONECTAR"
+ADDR=(SERVER,PORT)
+
+cliente=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cliente.connect(ADDR)
+
+def mandar(mensaje):
+    mensaje=mensaje.encode(FORMAT)
+    len_mensaje=len(mensaje)
+    mandar_len=str(len_mensaje).encode(FORMAT)
+    mandar_len += b' ' * (HEADER - len(mandar_len))
+    cliente.send(mandar_len)
+    cliente.send(mensaje)
